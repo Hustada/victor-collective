56d74fb0865bded5b55daa6c1803d13a329f9751
hustada <hustada@gmail.com>
2025-02-12 08:10:26 -0600

    WIP

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Box, Typography, Container, IconButton } from '@mui/material';
 import { motion } from 'framer-motion';
 import StarfieldBackground from './StarfieldBackground';
-import SpaceshipAnimation from './SpaceshipAnimation';
+
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 const Hero = () => {
@@ -25,7 +25,6 @@ const Hero = () => {
       }}
     >
       <StarfieldBackground />
-      <SpaceshipAnimation />
       
       <Container 
         maxWidth="md" 
